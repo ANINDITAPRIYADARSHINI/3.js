1a54fd7e8bab094a178d494e0d97434aa8054feb
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-05-28 20:50:53 +0530

    Update file structure & installed 3js.

---
+++ script2.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+
+
+//Create a scene
+
+
+//create a camera
+
+
+//create two objects
+
+
+
+//create a light
+
+
+//create a renderer
+
+
+//create an orbitControl
+
+
+
+//create animation
